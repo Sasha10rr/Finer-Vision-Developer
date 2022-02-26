00d6edaf1c6e726597e352deed43074d7e8daae7
Sasha10rr <59294074+Sasha10rr@users.noreply.github.com>
2022-02-26 12:19:01 +0100

    few changes

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const User = require("./models/User")
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'http://localhost:3001', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
@@ -29,6 +29,6 @@ app.post('/users',async (req,res) =>{
      
    
 })
-app.listen(8000,()=>{
+app.listen(8001,()=>{
     console.log("Server running")
 })
--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import axios from "axios" 
 import "./form.css"
 import { useRef } from 'react';
-import { useState } from 'react'
+import { useState } from 'react'  
+import Modal from "react-modal"
 export default function () {
 
   //getting value with useReg=f
@@ -15,7 +16,6 @@ export default function () {
   const month = useRef();
   const year = useRef();
   const comment = useRef();
-
 
   const[step1,setStep1] = useState("show")
   const[step2,setStep2] = useState("hide");
@@ -39,6 +39,7 @@ const handler3 =() =>{
   setStep2("hide")
   setStep1("hide") 
 }
+
 //show  step 2 on button click
 const handleNext = event =>{
   event.preventDefault();
@@ -54,12 +55,26 @@ const handleNext2 = event =>{
   setStep2("hide")
 }
 
+const backHandle =() =>{
+  if(step2==="show"){
+    setStep1("show")
+    setStep2("hide") 
+  }
+
+  else{
+    setStep3("hide")
+    setStep2("show") 
+  }
+}
+
+const [modalIsOpen, setModal] = useState(false)
+const [modalTitle,setModalTitle] =useState("Error")
+const [modalMessage,setModalMessage] = useState("Please Add The Missing Information In the Previous Steps")
 //submit buton to insert data
 const clickSubmit = async (e) =>{
   
   e.preventDefault();
-   
-console.log(gender.current.value)
+    
       const user = {
            firstname:firstName.current.value,
            surname:surname.current.value,
@@ -68,17 +83,22 @@ console.log(gender.current.value)
            gender:gender.current.value,
            birthdate:year.current.value+"-"+month.current.value+"-"+day.current.value,
            comment:comment.current.value
-      };
-      console.log(firstName.current.value)
+      }; 
+      
       
       try{
-          const res = await axios.post("http://localhost:8000/users",user); 
-          console.log(res.data)
+          const res = await axios.post("http://localhost:8001/users",user); 
+          console.log(user)
           if(res.data === "missing information"){
-            alert("missing information")
+            setModalMessage("Please Add The Missing Information In the Previous Steps")
+            setModalTitle("Error")
+            setModal(true)  
           }
-          else
-          alert("success")
+          else{  
+          setModalMessage("User Sucessfully added in the database!")
+          setModalTitle("Success") 
+          setModal(true)
+          }
       } catch (err){
           console.log(err);
       } 
@@ -87,6 +107,14 @@ console.log(gender.current.value)
 }
   return (
      <div className="form">
+       <Modal className="modal" isOpen={modalIsOpen}>
+     <h1 className={'modalTitle'+modalTitle}>{modalTitle}</h1>
+     <span className="modalMessage">{modalMessage}</span> 
+     <div className="buttons"> 
+     <button onClick={()=>setModal(false)}  className="modalClose">Close</button>
+
+     </div>
+    </Modal>
        <div className="formWrapper">
 
        <div className="steps">
@@ -127,52 +155,64 @@ console.log(gender.current.value)
               ref={email}
             /> 
             
-            <button  type='submit' className='next1'>Next</button>
+            <button  type='submit' className='next1'>Next > </button>
             </div>
           </form>
           </div>
 
           <div className="steps">
               <button className="title" onClick={handler2}>
-               <span className="details">Step 1: Your Details</span>
+               <span className="details">Step 2: More Comments</span>
              </button>
               <form className={"step2"+step2} onSubmit={handleNext2}>
               <div className="step1Form">
                 <div className='container'>
-                <label htmlFor="tel">Telephone Number</label>
+                <label htmlFor="tel"  >Telephone Number</label>
                 <input 
+                pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
                   required 
-                  type="number" 
+                  type="tel" 
+                  placeholder="123-4567-8901"
                   ref={number}
                 />
                 </div>
                 <div className='container'>
                 <label htmlFor="gender">Gender</label>
-                <select ref={gender}  id="gender"  > 
-                  <option selected disabled value="">Select Gender</option>
-                  <option value="Male" selected>Male</option>
+                <select required ref={gender} id="gender"  > 
+                  <option selected disabled value="" >Select Gender</option>
+                  <option value="Male" >Male</option>
                   <option value="Female">Female</option> 
                 </select>
                 </div>     
               </div> 
                 <div className='dateContainer'>
                 <label htmlFor="date">Date Of Birth</label>
-                <input ref={day} type="number" required placeholder='dd' max="31" maxLength="2" id="day" className='inputDate' /> 
-                <input ref={month} type="number" required placeholder='mm' max="12" maxLength="2" className='inputDate'   id="day"  /> 
+                <input ref={day} type="number" required placeholder='dd' max="31" min="1" maxLength="2" id="day" className='inputDate' /> 
+                <input ref={month} type="number" required placeholder='mm' max="12" min="1" maxLength="2" className='inputDate'   id="day"  /> 
                 <input ref={year} type="number" required placeholder='yyyy' min="1900" maxLength="4" className='inputDate'  id="day"  />
+                <button className='next'>Next ></button>
+                
+              <button className='goBackButton' onClick={backHandle}>Go Back</button>
               </div>
-              <button className='next'>Next</button>
+               
             </form>
           </div>
 
           <div className="steps">
             <button className="title" onClick={handler3}>
-               <span className="details">Step 1: Your Details</span>
+         
+               <span className="details">Step 3: Final Comments</span>
             </button>
+
             <form onSubmit={clickSubmit} className={"step3"+step3}>
-              <label htmlFor="comments">Comments</label>
-              <input required ref={comment} className='comments' type="text" />
-              <button className='submitButton'  >Submit</button>
+            <div className="step3Form">
+                <div className='container1'>
+              <label className='labelComment' htmlFor="comments">Comments</label>
+              <textarea required ref={comment} className='comments' type="text" />
+              </div>
+              </div> 
+              <button className='submitButton'>Submit</button>
+              <button type="button" className='goBackButton' onClick={backHandle}>Go Back</button>
             </form>
             
           </div>
@@ -181,5 +221,7 @@ console.log(gender.current.value)
          
        </div>
      </div>
+
+     
   )
 }
